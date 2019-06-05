a20491d5e27c2b7b63bc3f5879acb043da37f095
Andrey Serdyuk <messirleonard@gmail.com>
2019-06-05 16:21:12 +0400

    New

---
+++ InsertNumber/InsertNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace InsertNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte byte1 = 8;
+            byte byte2 = 15;
+            int i = 3;
+            int j = 8;
+
+
+
+            //rez |= 3;//занесли 3
+            //Console.WriteLine(rez);
+            //rez <<= 4;//сдвинули
+            //Console.WriteLine(rez);
+            //rez |= 5;//занесли 5
+            //Console.WriteLine(rez);//байт, что получили
+            //Console.WriteLine(rez & 0xF);//последнее число 0xF~"1111"
+            //rez >>= 4;//сдвинули
+            //Console.WriteLine(rez & 0xF);//первое число
+        }
+    }
+}
